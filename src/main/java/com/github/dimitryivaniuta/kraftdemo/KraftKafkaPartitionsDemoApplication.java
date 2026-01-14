11058c147e7d02c84236148f113eaf8b532a3e8c
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2026-01-14 18:38:07 +0100

    Kafka Application

---
+++ src/main/java/com/github/dimitryivaniuta/kraftdemo/KraftKafkaPartitionsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.dimitryivaniuta.kraftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraftKafkaPartitionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KraftKafkaPartitionsDemoApplication.class, args);
+    }
+}
